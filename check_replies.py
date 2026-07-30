7a792dfd73b3b9043878d0b47a5f1b100ef2e78d
willjackherold-boop <willjackherold@gmail.com>
2026-07-30 17:51:30 +0100

    Update check_replies.py

--- check_replies.py
+++ check_replies.py
@@ -1,1 +1,12 @@
+import json
+import os
+import requests
 
+TOKEN = os.environ["BOT_TOKEN"]
+
+url = f"https://api.telegram.org/bot{TOKEN}/getUpdates"
+
+response = requests.get(url)
+
+print("Status:", response.status_code)
+print(json.dumps(response.json(), indent=4))
